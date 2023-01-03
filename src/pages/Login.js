8456d19f32ca5f10a1b3c271678671144163f5d4
Dharmendra <dharmendrasingh75039@gmail.com>
2023-01-03 18:04:15 +0530

    useProvideAuth hooks

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 import styles from '../styles/login.module.css';
 import { useToasts } from 'react-toast-notifications';
-import { login } from '../api';
+import { useAuth } from '../hooks';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [loggingIn, setLoggingIn] = useState(false);
   const { addToast } = useToasts();
+  const auth = useAuth();
+  console.log(auth);
 
   const handleSubmit = async (e) => {
     //we don't want to reload the page on submit
@@ -20,10 +22,10 @@ const Login = () => {
       });
     }
 
-    const response = await login(email, password);
+    const response = await auth.login(email, password);
 
     if (response.success) {
-      addToast('Successfully loffed in', {
+      addToast('Successfully logged in', {
         appearance: 'success',
       });
     } else {
