fee95aed8507e1026dc3e4cd34a981792be439f5
Dharmendra <dharmendrasingh75039@gmail.com>
2023-01-09 14:13:16 +0530

    improving user profile logic

--- src/components/App.js
+++ src/components/App.js
@@ -22,6 +22,7 @@ const Page404 = () => {
 
 function App() {
   const auth = useAuth();
+  // console.log(auth);
 
   if (auth.loading) {
     return <Loader />;
--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,12 +1,52 @@
 import styles from '../styles/settings.module.css';
+import { useToasts } from 'react-toast-notifications';
+import { useParams, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { fetchUserProfile } from '../api';
+import { Loader } from '../components';
 import { useAuth } from '../hooks';
-import { useLocation } from 'react-router-dom';
 
 const UserProfile = () => {
-  const { state } = useLocation();
-  // console.log('location', state);
+  //const { state } = useLocation()
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
+  const { userId } = useParams();
+  const { addToast } = useToasts();
+  const navigate = useNavigate();
+  const auth = useAuth();
 
-  const { user = {} } = state;
+  useEffect(() => {
+    const getUser = async () => {
+      const response = await fetchUserProfile(userId);
+      if (response.success) {
+        setUser(response.data.user);
+      } else {
+        addToast(response.message, {
+          appearance: 'error',
+        });
+        return navigate('/');
+      }
+      setLoading(false);
+    };
+    getUser();
+  }, [userId, navigate, addToast]);
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  const checkIfUserIsAFriend = () => {
+    const friends = auth.user.friendships;
+    // give Array of ids
+    const friendIds = friends.map((friend) => friend.to_user._id);
+    const index = friendIds.indexOf(userId);
+
+    if (index !== -1) {
+      return true;
+    }
+    return false;
+  };
+
   return (
     <div className={styles.settings}>
       <div className={styles.imgContainer}>
@@ -27,8 +67,11 @@ const UserProfile = () => {
       </div>
 
       <div className={styles.btnGrp}>
-        <button className={`button ${styles.saveBtn}`}>Add Friend</button>
-        <button className={`button ${styles.saveBtn}`}>Remove Friend</button>
+        {checkIfUserIsAFriend() ? (
+          <button className={`button ${styles.saveBtn}`}>Remove Friend</button>
+        ) : (
+          <button className={`button ${styles.saveBtn}`}>Add Friend</button>
+        )}
       </div>
     </div>
   );
