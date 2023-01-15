8121f3a4d4024c1b7b294aa9a94fdc4e03cd3641
Dharmendra <dharmendrasingh75039@gmail.com>
2023-01-15 13:08:18 +0530

    creating friendList

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import App from './App';
 import Loader from './Loader';
 import Comment from './Comment';
 import Navbar from './Navbar';
+import FriendsList from './FriendsList';
 
-export { App, Loader, Comment, Navbar };
+export { App, Loader, Comment, Navbar, FriendsList };
